15dea183fd1ce15584ec4b9c6efa4910c190593c
BuatBelajar1290 <132242541+BuatBelajar1290@users.noreply.github.com>
2023-05-03 20:45:29 +0700

    commit 2

--- src/handler.js
+++ src/handler.js
@@ -36,5 +36,34 @@ const addNoteHandler = (request, h) => {
     return response;
 };
 
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+      notes,
+    },
+  });
 
-module.exports = { addNoteHandler };
+  const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const note = notes.filter((n) => n.id === id)[0];
+
+    if (note !== undefined) {
+        return {
+          status: 'success',
+          data: {
+            note,
+          },
+        }; 
+    }
+    
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan tidak ditemukan',
+    });
+      response.code(404);
+      return response;
+  };
+
+  
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
